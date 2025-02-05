76c2a85f33cb1249c8dbac25343db4e37195140e
Daniel <daniel_academico1998@hotmail.com>
2025-02-05 09:12:56 -0300

    Add initial project files and configurations for Flutter application

---
+++ android/app/src/main/kotlin/com/danielbrown/alura_web_api_app_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.danielbrown.alura_web_api_app_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Journal',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: AppBarTheme(
+          elevation: 2,
+          backgroundColor: Colors.black87,
+          titleTextStyle: TextStyle(
+            color: Colors.white70,
+          ),
+        ),
+        textTheme: GoogleFonts.bitterTextTheme(),
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+      },
+    );
+  }
+}
