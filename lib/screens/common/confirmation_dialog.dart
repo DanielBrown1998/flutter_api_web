19c5fb5a6169ddb02503af7071441272fb8f6c80
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-02-20 19:36:21 -0300

    Refactor confirmation dialog for improved readability; update db.json to include user data structure

--- lib/screens/common/confirmation_dialog.dart
+++ lib/screens/common/confirmation_dialog.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
 
 Future<dynamic> showConfirmationDialog(
-  BuildContext context, 
-  {String title = 'Atençao', 
-  String content="Você deseja executar esta ação?", 
+  BuildContext context, {
+  String title = 'Atençao',
+  String content = "Você deseja executar esta ação?",
   String confirmation = 'Confirmar',
   String cancel = 'Cancelar',
-  }){
+}) {
   return showDialog(
-    context: context, 
-    builder: (context) {  
-      return AlertDialog(
-        title: Text(title),
-        content: Text(content),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context, false);
-            }, 
-            child: Text(cancel)
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context, true);
-            }, 
-            child: Text(confirmation)
-          ),
-        ],
-      );
-    });
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+                child: Text(cancel)),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context, true);
+                },
+                child: Text(confirmation)),
+          ],
+        );
+      });
 }
